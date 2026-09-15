b5a7e0dd7c2706f279e588c0044bff92223862ac
Fares Khader <Fareskhader52@gmail.com>
2026-09-15 14:16:41 +0300

    add hero section + install needed library

--- src/conponents/hero/Hero.jsx
+++ src/conponents/hero/Hero.jsx
@@ -1,13 +1,24 @@
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 
-import testimg from './../../assets/AI Grid.svg'
+import HeroPhoto from './../../assets/HeroPhoto.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
- function BasicButtons() {
+
+function BasicButtons() {
   return (
-    <div >
-     <h1>hero</h1>
-      <img src={testimg} />
+    <div className='green_background vh-100'>
+    <div className=' container d-flex flex-column align-items-center vh-100 justify-content-center text-white gap-3' >
+      
+      <img className=' w-20' src={HeroPhoto} />
+           <h1 className=' heading_font_size custom-font fw-bold text-uppercase pt-3'>Start Bootstrap</h1>
+        <div className='divider-custom'>
+          <span className='divider-custom-line'></span>
+          <FontAwesomeIcon className='divider-custom-icon' icon={faStar} />
+          <span className='divider-custom-line'></span>
+        </div>
+        <p className='lato-font fs-3'>Graphic Artist - Web Designer - Illustrator</p>
+
+    </div>
     </div>
   );
 }
